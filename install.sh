2508767092b29c9c031f25c2c7c18f031ea00eb2
Michael Lawrenson <ctrlcmdspace@gmail.com>
2021-05-03 13:12:38 -0400

    Add install file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# 
+# RASPBERRY PI INSTALLATION SCRIPT
+# 
+# curl https://raw.githubusercontent.com/minelminel/microbot/master/microbot/install.sh | sudo bash
+# 
+set -e
+
+# create the source folder, if it does not already exist
+
+# try to clone the repo, if it fails perform a git pull
+
+# install system requirements
+
+# install python3 & venv
+
+# create a virtualenv, source
+
+# install package
+
+# run tests
+
+# create config file if it does not already exist
+
+# 
