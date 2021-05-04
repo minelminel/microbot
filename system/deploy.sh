773353a473c47ad7901681dff5c96a2f9e6e4804
Michael Lawrenson <ctrlcmdspace@gmail.com>
2021-05-04 00:08:42 -0400

    User adjustment

--- system/deploy.sh
+++ system/deploy.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y nginx
 sudo cp ./microbot.* /etc/systemd/system/
 sudo cp ./nginx.conf /etc/nginx/nginx.conf
 sudo systemctl enable --now microbot.service
+sudo systemctl restart microbot.service
 sudo systemctl enable --now nginx.service
 curl localhost:8000
